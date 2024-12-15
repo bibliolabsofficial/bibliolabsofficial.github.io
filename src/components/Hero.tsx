00e53775231fdb38bbf11c669af544a9e1d95c19
Filipe Ramos <1mybackupone@gmail.com>
2024-12-14 23:55:25 -0300

    feat: Add resposiveness to hero and appShowcase

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
       <div className="hero__wrapper | wrapper">
         <h2 className="hero__title">The Art of War</h2>
         <h3 className="hero__subtitle">Sun Tzu</h3>
-        <p>
+        <p className='hero__description'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
           reiciendis sed error doloribus ex accusamus dicta excepturi nesciunt,
           voluptatibus hic impedit aliquid accusantium laboriosam ab tenetur
