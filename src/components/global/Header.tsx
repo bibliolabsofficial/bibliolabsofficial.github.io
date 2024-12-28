63c4883e7d5084352844ba89c9f6c658f6fd16f5
Filipe Ramos <146658398+filipe-2@users.noreply.github.com>
2024-12-28 15:41:54 -0300

    Update Header.tsx

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -138,7 +138,7 @@ export default function Header() {
             onClick={toggleMenuVisibility}
           >
             <span className='sr-only'>menu</span>
-            <span className="fa fa-bars" aria-hidden="true"></span>
+            <span className=className={`fa fa-${menuOpened ? 'x' : 'bars'}`} aria-hidden="true"></span>
           </button>
 
           <ul
